18551a296af60577395fef40cdddc718b9138856
rjmendezl <00103719@uca.edu.sv>
2023-06-09 21:44:02 -0600

    :tada: Add: Base project has been added.

---
+++ RySTransports/src/main/java/com/rys/rystransports/RySTransportsApplication.java
@@ -0,0 +1,13 @@
+package com.rys.rystransports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RySTransportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RySTransportsApplication.class, args);
+	}
+
+}
---
+++ RySTransports/src/test/java/com/rys/rystransports/RySTransportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rys.rystransports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RySTransportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
